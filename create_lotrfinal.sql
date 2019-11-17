bb168fc7e9460666c64fcc133e223381a0b88763
Nickhil Tekwani <44220251+nickhil-tekwani@users.noreply.github.com>
2019-11-16 20:27:29 -0500

    updated create_lotrfinal

--- create_lotrfinal.sql
+++ create_lotrfinal.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE  IF NOT EXISTS `lotrfinal` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `lotrfinal` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `lotrfinal`;
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.17, for macos10.14 (x86_64)
 --
--- Host: 127.0.0.1    Database: lotrfinal
+-- Host: localhost    Database: lotrfinal
 -- ------------------------------------------------------
--- Server version	5.7.12-log
+-- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,7 +23,7 @@ USE `lotrfinal`;
 
 DROP TABLE IF EXISTS `lotr_book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lotr_book` (
   `book_id` int(11) NOT NULL,
   `title` varchar(255) DEFAULT NULL,
@@ -47,7 +47,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `lotr_character`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lotr_character` (
   `character_name` varchar(255) NOT NULL,
   `species` varchar(255) DEFAULT NULL,
@@ -81,7 +81,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `lotr_first_encounter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lotr_first_encounter` (
   `character1_name` varchar(255) NOT NULL,
   `character2_name` varchar(255) NOT NULL,
@@ -114,7 +114,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `lotr_region`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lotr_region` (
   `region_name` varchar(255) NOT NULL,
   `major_species` varchar(255) DEFAULT NULL,
@@ -143,7 +143,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `lotr_species`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lotr_species` (
   `species_name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -163,8 +163,35 @@ INSERT INTO `lotr_species` VALUES ('balrog','Ancient evil creatures',5),('dwarf'
 UNLOCK TABLES;
 
 --
+-- Dumping events for database 'lotrfinal'
+--
+
+--
 -- Dumping routines for database 'lotrfinal'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `track_character` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `track_character`(IN character_input CHAR(20))
+BEGIN
+    SELECT character_name, character2_name, region_name
+    FROM lotr_character
+    INNER JOIN lotr_first_encounter
+    ON lotr_character.character_name = lotr_first_encounter.character1_name
+    WHERE character1_name=character_input OR character2_name=character_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -175,4 +202,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-04 12:37:55
+-- Dump completed on 2019-11-16 20:25:05
